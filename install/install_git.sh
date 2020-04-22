461cb4aa01728f7f858f9b7af3091d452ad6101e
mstormo <mstormo@gmail.com>
2020-04-21 20:17:00 -0500

    Force OpenSSL with TLS v1.2 support

--- install/install_git.sh
+++ install/install_git.sh
@@ -3,12 +3,15 @@
 curldev=libcurl-devel.x86_64
 [ $1 -eq 5 ] && curldev=curl-devel.x86_64
 
+openssldev=openssl-devel.x86_64
+[ $1 -eq 5 ] && openssldev="openssl1-devel-1.0.1e-57 wget-1.12-8.1.el5_11 --exclude=\"openssl-devel-0.9.8e-40.el5_11.1\""
+
 # install GIT to a recent version -----------------------------------------------------------------
-yum install -y \
+yum install -y -t --nogpgcheck \
         $curldev \
         expat-devel.x86_64 \
         gettext-devel.x86_64 \
-        openssl-devel.x86_64
+        $openssldev
 
 [ $1 -lt 7 ] && source scl_source enable devtoolset-2 2>/dev/null || echo GCC 4.8 enabled
 
